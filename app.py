90c003012efbb36ef9e7229ff571ab8a99c1535c
hunixoo <nvtuyen1803@gmail.com>
2025-07-09 19:28:03 +0700

    initial commit

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def render_invoice():
 
     img = img.crop((0, 0, width, y + 10))
 
-    escpos_data = image_to_escpos(img)
+    escpos_data = image_to_raster_escpos(img)
     return Response(escpos_data, mimetype='application/octet-stream')
 
 
@@ -80,6 +80,33 @@ def image_to_escpos(img: Image.Image) -> bytes:
 
     bytes_data += b'\x0A\x0A\x1D\x56\x00'  # feed + cut
     return bytes(bytes_data)
+
+
+def image_to_raster_escpos(img: Image.Image) -> bytes:
+    img = img.convert('1')  # chuyển ảnh về 1-bit B/W
+    width, height = img.size
+    width_bytes = (width + 7) // 8
+    data = bytearray()
+
+    # Header: GS v 0
+    data += b'\x1D\x76\x30\x00'  # GS v 0 m=0
+    data += bytes([width_bytes % 256, width_bytes // 256])
+    data += bytes([height % 256, height // 256])
+
+    for y in range(height):
+        for x_byte in range(width_bytes):
+            byte = 0x00
+            for bit in range(8):
+                x = x_byte * 8 + bit
+                if x < width:
+                    pixel = img.getpixel((x, y))
+                    if pixel == 0:
+                        byte |= (1 << (7 - bit))
+            data.append(byte)
+
+    # Thêm line feed và cut
+    data += b'\x0A\x0A\x1D\x56\x00'
+    return bytes(data)
 
 
 if __name__ == '__main__':
